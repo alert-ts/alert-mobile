10a78a19e0f71a10835102120465a5c3bb6d07cd
Raisess <raisechif@gmail.com>
2021-05-23 13:28:32 -0300

    created views base

---
+++ src/views/Feed.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout, Text } from "@ui-kitten/components";
+
+export default function Feed(): JSX.Element {
+  return (
+    <Layout
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Feed view</Text>
+    </Layout>
+  );
+}
---
+++ src/views/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout, Text } from "@ui-kitten/components";
+
+export default function Search(): JSX.Element {
+  return (
+    <Layout
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Search view</Text>
+    </Layout>
+  );
+}
